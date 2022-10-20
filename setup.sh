5625b115f802e84ae46d57d7fac7ebffcbc3f0c3
Arash Pakbin <a.pakbin@cse-stmi-s2.engr.tamu.edu>
2022-10-20 15:27:07 -0500

    new setup using pip

--- setup.sh
+++ setup.sh
@@ -32,34 +32,27 @@ is_mac=$((1-$is_mac))
 is_linux=$((1-$is_linux))
 
 
+build_dirs=(
+    "${DIR}/packages/boxhed/boxhed.egg-info/"
+    "${DIR}/packages/boxhed/build/"
+    "${DIR}/packages/boxhed_kernel/boxhed_kernel/build/"
+    "${DIR}/packages/boxhed_kernel/boxhed_kernel/lib/"
+    "${DIR}/packages/boxhed_kernel/boxhed_kernel/python-package/build/"
+    "${DIR}/packages/boxhed_kernel/boxhed_kernel/python-package/xgboost.egg-info/"
+    "${DIR}/packages/boxhed_prep/build/"
+    "${DIR}/packages/boxhed_prep/boxhed_prep.egg-info/") 
+
+for dir in ${build_dirs[@]}; 
+do
+    rm -rf $dir
+done
+
+
 use_gpu=false
 while getopts ":gv:" opt; do
     case $opt in
         g)
             use_gpu=true
-            ;;
-        v)
-            str=""
-            if [[ $is_windows == 0 ]]; then
-                echo "ERROR: The -$opt option should only be used in Windows when specifying the Visual Studio version."
-                exit 1
-            fi
-            case $OPTARG in
-                14)
-                    str+="\"Visual Studio 14 2015 Win64\""
-                    ;;
-                15)
-                    str+="\"Visual Studio 15 2017\" -A x64"
-                    ;;
-                16)
-                    str+="\"Visual Studio 16 2019\" -A x64"
-                    ;;
-                *)
-                    echo "ERROR: Invalid VS version after the -$opt option. It can be either 14, 15, or 16." >&2
-                    exit 1
-                    ;;
-            esac
-            cmake_args+=(-G${str})
             ;;
         \?)
             echo "ERROR: Invalid option: -$OPTARG" >&2
@@ -80,6 +73,7 @@ then
         exit 1
     fi
     cmake_args+=(-DUSE_CUDA=ON)
+    cmake_args+=(-D CMAKE_CUDA_COMPILER=/home/grads/a/a.pakbin/cuda-11.1/bin/nvcc)
 else
     cmake_args+=(-DUSE_CUDA=OFF)
 fi
@@ -88,8 +82,8 @@ rm -f setup_log
 
 echo "This installation relies heavily on https://xgboost.readthedocs.io/en/latest/build.html"
 
-print "creating build directory for boxhed2.0 in ${DIR}/boxhed.kernel/"
-cd "${DIR}/boxhed.kernel/"
+print "creating build directory for boxhed2.0 in ${DIR}/packages/boxhed_kernel/"
+cd "${DIR}/packages/boxhed_kernel/boxhed_kernel/"
 mkdir -p build &> ${setup_log}
 check_success
 
@@ -99,13 +93,13 @@ if [[ $is_mac == 1 ]]; then
     check_success
 fi
 
-print "running cmake for boxhed in ${DIR}/boxhed.kernel/build/"
-cd "${DIR}/boxhed.kernel/build/"
+print "running cmake for boxhed in ${DIR}/packages/boxhed_kernel/boxhed_kernel/build/"
+cd "${DIR}/packages/boxhed_kernel/boxhed_kernel/build/"
 cmake ${cmake_args[@]} >> ${setup_log} 2>&1
 check_success
 
-print "running make for boxhed in ${DIR}/boxhed.kernel/build/"
-cd "${DIR}/boxhed.kernel/build/"
+print "running make for boxhed in ${DIR}/packages/boxhed_kernel/boxhed_kernel/build/"
+cd "${DIR}/packages/boxhed_kernel/boxhed_kernel/build/"
 build_args=(
     --build .
 )
@@ -115,8 +109,8 @@ fi
 cmake ${build_args[@]}  >> ${setup_log} 2>&1
 check_success
 
-print "setting up boxhed for python in ${DIR}/boxhed.kernel/python-package/"
-cd "${DIR}/boxhed.kernel/python-package/"
+print "setting up boxhed for python in ${DIR}/packages/boxhed_kernel/boxhed_kernel/python-package/"
+cd "${DIR}/packages/boxhed_kernel/boxhed_kernel/python-package/"
 python setup.py install >> ${setup_log} 2>&1
 check_success
 
@@ -125,19 +119,14 @@ print "boxhed installed successfully"
 
 ####### setting up preprocessing #######
 
-print "creating build directory for preprocessor in ${DIR}/build/"
-cd "${DIR}"
-mkdir -p build >> ${setup_log} 2>&1
-check_success
-
-print "running cmake for preprocessor in ${DIR}/build/"
-cd "${DIR}/build/"
-cmake ../preprocessor_installer  >> ${setup_log} 2>&1
-check_success
-
-print "running cmake --build for preprocessor in ${DIR}/build/"
-cmake --build .  >> ${setup_log} 2>&1
-check_success
+print "installing preprocessor"
+cd "${DIR}/packages/"
+pip install ./boxhed_prep
 
 
-print "preprocessor installed successfully"
+####### setting up boxhed #######
+
+print "installing BoXHED"
+cd "${DIR}/packages/"
+pip install ./boxhed
+
