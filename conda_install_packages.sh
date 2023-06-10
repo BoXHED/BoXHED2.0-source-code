1b06550d34b0db09d0a6b6a1c35041f6de77f9b3
Arash Pakbin <a.pakbin@tamu.edu>
2023-06-10 15:44:48 -0500

    Update conda_install_packages.sh
    lumping lines together

--- conda_install_packages.sh
+++ conda_install_packages.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
 
-conda install -c anaconda pandas=1.5.2 -y
-conda install -c anaconda numpy -y
-conda install -c anaconda scikit-learn -y
-conda install -c anaconda pytz -y
-conda install -c conda-forge py3nvml -y
-conda install -c conda-forge matplotlib -y
-conda install jupyter -y
-conda install -c anaconda cmake -y
-conda install -c conda-forge cudatoolkit -y
+conda install -c anaconda pandas=1.5.2 numpy scikit-learn pytz jupyter cmake -y
+conda install -c conda-forge py3nvml matplotlib -y
