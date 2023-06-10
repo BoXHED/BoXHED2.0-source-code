1350c32db0a6b783c42a5bb319a372e2ec625464
Arash Pakbin <a.pakbin@tamu.edu>
2023-06-10 15:35:36 -0500

    Update conda_install_packages.sh
    updated version for pandas package

--- conda_install_packages.sh
+++ conda_install_packages.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
+conda install -c anaconda pandas=1.5.2 -y
 conda install -c anaconda numpy -y
-conda install -c anaconda pandas -y
 conda install -c anaconda scikit-learn -y
 conda install -c anaconda pytz -y
 conda install -c conda-forge py3nvml -y
