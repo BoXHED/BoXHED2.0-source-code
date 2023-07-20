4a478bec842393e13266cf3e51b4e8cd1c6e11e5
Arash Pakbin <a.pakbin@cse-stmi-s2.engr.tamu.edu>
2023-07-19 21:45:58 -0500

    versioning

--- packages/boxhed_kernel/boxhed_kernel/python-package/setup.py
+++ packages/boxhed_kernel/boxhed_kernel/python-package/setup.py
@@ -288,7 +288,7 @@ if __name__ == '__main__':
     # - python setup.py develop   # same as above
     logging.basicConfig(level=logging.INFO)
     setup(name='boxhed_kernel',
-          version="2.0.1",#open(os.path.join(
+          version="2.0.2",#open(os.path.join(
           #    CURRENT_DIR, 'boxhed_kernel/VERSION')).read().strip(),
           description="XGBoost Python Package",
           long_description=open(os.path.join(CURRENT_DIR, 'README.rst'),
