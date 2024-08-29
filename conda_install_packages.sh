7b99baf2311fb44dfc879034ada81f03ab60916f
Arash Pakbin <a.pakbin@tamu.edu>
2024-08-29 16:56:22 -0500

    Add files via upload

---
+++ conda_install_packages.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+conda install -c anaconda pandas=1.5.2 numpy=1.24.3 scikit-learn=1.2.2 pytz=2022.7 jupyter -y
+conda install -c conda-forge matplotlib=3.7.1 -y
+pip install pandas==1.5.2
+pip install cmake==3.26.3
+pip install py3nvml==0.2.7
